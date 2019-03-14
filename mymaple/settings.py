3faff0e462d8321469d1f8d35117493aad403632
andyywz <andyywz@gmail.com>
2019-03-14 01:34:31 -0400

    update settings

--- mymaple/settings.py
+++ mymaple/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
         'NAME': 'mymaple',
         'HOST': '127.0.0.1',
         'POST': '5432',
-        'USER': 'ayong',
-        'PASSWORD': ''
+        'USER': 'postgres',
+        'PASSWORD': 'Password*8'
     }
 }
 
